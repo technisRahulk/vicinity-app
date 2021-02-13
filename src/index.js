f50a89bf94270dc3d062f450d80d1f1e36a0ac7a
technisRahulk <rahulkumaru99@gmail.com>
2021-02-14 00:33:34 +0530

    contact-form backend added

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ const trackData = require("./utils/finalAlgo");
 const Admin=require('./models/admin')
 const auth=require('./middleware/auth')
 const cookieParser = require("cookie-parser");
+const multiparty = require("multiparty");
+const nodemailer = require("nodemailer");
 
 //Express server setup
 const app = express();
@@ -1109,6 +1111,53 @@ app.post("/searchGlobal", upload.single('file1'), async (req, response) => {
 
 })
 
+//contact-us form 
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+// verify connection configuration
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+//contact-us route 
+app.post("/send", (req, res) => {
+  //1.
+  let form = new multiparty.Form();
+  let data = {};
+  form.parse(req, function (err, fields) {
+    console.log(fields);
+    Object.keys(fields).forEach(function (property) {
+      data[property] = fields[property].toString();
+    });
+
+    //2. You can configure the object however you want
+    const mail = {
+      from: data.name,
+      to: process.env.EMAIL,
+      subject: data.subject,
+      text: `${data.name} <${data.email}> \n${data.message}`,
+    };
+
+    //3.
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Something went wrong.");
+      } else {
+        res.status(200).send("Email successfully sent to recipient!");
+      }
+    });
+  });
+});
 
 
 
